7d7c36b9bd5a8fa58ac901dc8e790d59964c0a87
Marek Langiewicz <(marek.langiewicz@gmail.com)>
2016-04-13 15:52:48 +0200

    build file updated

--- app/src/main/java/pl/mareklangiewicz/myintentsample/Fragment2.kt
+++ app/src/main/java/pl/mareklangiewicz/myintentsample/Fragment2.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import pl.mareklangiewicz.mycorelib.MyClass
 import pl.mareklangiewicz.myfragments.MyFragment
 
 class Fragment2 : MyFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        MyClass.bla()
+
         manager?.lnav?.menuId = R.menu.local_menu2
         return inflater.inflate(R.layout.fragment2, container, false)
     }
